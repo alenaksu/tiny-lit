355f20862af16ff6adad97eccc8e990f19dfc8cf
Alessandro Valeri (alenaksu) <misterroot@gmail.com>
2018-11-17 00:05:30 +0100

    chore: update publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 read -p 'OTP: ' otp
-NPM_CONFIG_OTP=$otp
-lerna publish --npm-client=npm --force-publish=* --sign-git-commit --sign-git-tag --registry https://registry.npmjs.org/
+
+NPM_CONFIG_OTP=$otp lerna publish --npm-client=npm --force-publish=* --sign-git-commit --sign-git-tag --registry https://registry.npmjs.org/
