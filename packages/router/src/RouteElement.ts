8766d36c3dbd0765428b9cc49380654f859c3434
Alessandro Valeri (alenaksu) <misterroot@gmail.com>
2019-02-09 02:03:46 +0100

    fix(router): remove dynamic import

--- packages/router/src/RouteElement.ts
+++ packages/router/src/RouteElement.ts
@@ -1,14 +1,23 @@
 import { Router, RouteComponent } from './types';
 import { requestRouter } from './events';
 
-const waitComponent = (
-    route: RouteElement,
-    component: HTMLElement
-) =>
-    Promise.resolve(
-        !route.hasAttribute('module') ||
-        import(route.getAttribute('module')!)
-    )
+const importedModules = new Set();
+const importModule = url =>
+    new Promise(resolve => {
+        if (!url || importedModules.has(url)) return resolve();
+        else {
+            const script = document.createElement('script');
+            script.onload = resolve;
+            script.src = url;
+
+            document.appendChild(script);
+
+            importedModules.add(url);
+        }
+    });
+
+const waitComponent = (route: RouteElement, component: HTMLElement) =>
+    importModule(route.getAttribute('module')!)
         // TODO check element upgrade
         .then(() => customElements.whenDefined(component.localName));
 
@@ -29,28 +38,29 @@ export class RouteElement extends HTMLElement {
 
             this.dispose = this.router.on(path, {
                 onEnter: params => {
-                    waitComponent(
-                        this,
-                        component
-                    ).then(() =>
-                        this.appendChild(component)
-                    ).then(() => component.onRouteEnter && component.onRouteEnter(params));
+                    waitComponent(this, component)
+                        .then(() => this.appendChild(component))
+                        .then(
+                            () =>
+                                component.onRouteEnter &&
+                                component.onRouteEnter(params)
+                        );
                 },
                 onUpdate: params => {
-                    waitComponent(
-                        this,
-                        component
-                    ).then(() => component.onRouteUpdate && component.onRouteUpdate(params));
+                    waitComponent(this, component).then(
+                        () =>
+                            component.onRouteUpdate &&
+                            component.onRouteUpdate(params)
+                    );
                 },
                 onLeave: () => {
-                    waitComponent(
-                        this,
-                        component
-                    )
-                        .then(() => component.onRouteLeave && component.onRouteLeave())
-                        .then(() =>
-                            this.removeChild(component)
-                        );
+                    waitComponent(this, component)
+                        .then(
+                            () =>
+                                component.onRouteLeave &&
+                                component.onRouteLeave()
+                        )
+                        .then(() => this.removeChild(component));
                 }
             });
         }
